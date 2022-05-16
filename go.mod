6b9563dbfcfc1036abbef73953cff42aeb1c6660
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-05-15 20:46:19 -0400

    Add REST server

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/yescorihuela/microservice-milestone-kafka-2
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/render v1.0.1
+)
--- main.go
+++ main.go
@@ -1,17 +1,102 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
+
+var mainTopic string = "OrderReceived"
+
+type Saludo struct {
+	Hola string `json:"hola"`
+}
+
+type ErrResponse struct {
+	Err            error `json:"-"` // low-level runtime error
+	HTTPStatusCode int   `json:"-"` // http response status code
+
+	StatusText string `json:"status"`          // user-level status message
+	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func ErrInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
+}
+
+func kafkaProducer(message []byte) {
+	fmt.Println("Starting producer...")
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &mainTopic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, nil)
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+}
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
-	r.Get("/hola", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Use(middleware.Logger)
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+	r.Post("/order-received", func(w http.ResponseWriter, r *http.Request) {
+		data := &Saludo{}
+		if err := json.NewDecoder(r.Body).Decode(data); err != nil {
+			render.Render(w, r, ErrInvalidRequest(err))
+			return
+		}
+
+		t, _ := json.Marshal(data)
+		kafkaProducer(t)
+		w.WriteHeader(http.StatusCreated)
 	})
 	http.ListenAndServe(":3000", r)
 }
